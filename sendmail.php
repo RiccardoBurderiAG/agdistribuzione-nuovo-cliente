0b4bc2fd1e6745b271a43e86dda1a733db311ed5
RiccardoBurderiAG <riccardo.burderi@aghoreca.com>
2024-02-15 12:16:30 +0100

    feat: added style for gmail view

--- sendmail.php
+++ sendmail.php
@@ -61,55 +61,55 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
 			</style>
 		</head>
 		<body>
-			<table>
-			<tr><td><img src='https://www.agdistribuzione.it/images/template/agdistribuzione.jpg' width='200' /></td>
+			<table style=\"font-family: arial, sans-serif;border-collapse: collapse;width: 100%;\">
+			<tr><td><img src='https://www.agdistribuzione.it/images/template/agdistribuzione.jpg' width='150' /></td>
 			<td><h3>Inserimento Nuovo Cliente</h3></td></tr>
-			<tr><td colspan='2'><h3>Dettagli Agente, Listino e Condizioni di Pagamento</h3></td></tr>
+			<tr style=\"background-color: #dddddd;\"><td colspan='2'><h3>Dettagli Agente, Listino e Condizioni di Pagamento</h3></td></tr>
 			<tr><td>Codice Agente</td> <td>$codice_agente</td></tr>
-			<tr><td>Listino</td> <td>$listino</td></tr>
+			<tr style=\"background-color: #dddddd;\"><td>Listino</td> <td>$listino</td></tr>
 			<tr><td>Condizioni Pagamento</td> <td>$condizioni_pagamento</td></tr>
-			<tr><td>Email Agente</td> <td>$email_agente</td></tr>
+			<tr style=\"background-color: #dddddd;\"><td>Email Agente</td> <td>$email_agente</td></tr>
 
 			<tr><td colspan='2'><h3>Dettagli Cliente</h3></td></tr>
-			<tr><td>Tipologia Azienda</td> <td>$agency_type</td></tr>
+			<tr style=\"background-color: #dddddd;\"><td>Tipologia Azienda</td> <td>$agency_type</td></tr>
 			<tr><td>Titolare</td> <td>$titolare</td></tr>
-			<tr><td>Denominazione Commerciale</td> <td>$denominazione</td></tr>
+			<tr style=\"background-color: #dddddd;\"><td>Denominazione Commerciale</td> <td>$denominazione</td></tr>
 			<tr><td>Tipo di Attivit&agrave;</td> <td>$attivita</td></tr>
-			<tr><td>Tel</td> <td>$tel</td></tr>
+			<tr style=\"background-color: #dddddd;\"><td>Tel</td> <td>$tel</td></tr>
 			<tr><td>Email</td> <td>$email_cliente</td></tr>
-			<tr><td>PEC</td> <td>$pec</td></tr>
+			<tr style=\"background-color: #dddddd;\"><td>PEC</td> <td>$pec</td></tr>
 
 			<tr><td colspan='2'><h3>Indirizzo di Fatturazione</h3></td></tr>
-			<tr><td>Partita Iva</td> <td>$vat</td></tr>
+			<tr style=\"background-color: #dddddd;\"><td>Partita Iva</td> <td>$vat</td></tr>
 			<tr><td>Codice Univoco</td> <td>$unique_code</td></tr>
-			<tr><td>Indirizzo Fatturazione</td> <td>$address</td></tr>
+			<tr style=\"background-color: #dddddd;\"><td>Indirizzo Fatturazione</td> <td>$address</td></tr>
 			<tr><td>Cap</td> <td>$cap</td></tr>
-			<tr><td>Citt&agrave;</td> <td>$city</td></tr>
+			<tr style=\"background-color: #dddddd;\"><td>Citt&agrave;</td> <td>$city</td></tr>
 			<tr><td>Provincia</td> <td>$province</td></tr>
 
 
-			<tr><td colspan='2'><h3>Indirizzo di Consegna</h3></td></tr>
+			<tr style=\"background-color: #dddddd;\"><td colspan='2'><h3>Indirizzo di Consegna</h3></td></tr>
 			<tr><td>Indirizzo Destinazione Merci</td> <td>$merci_address</td></tr>
-			<tr><td>Cap</td> <td>$merci_cap</td></tr>
+			<tr style=\"background-color: #dddddd;\"><td>Cap</td> <td>$merci_cap</td></tr>
 			<tr><td>Citt&agrave;</td> <td>$merci_city</td></tr>
-			<tr><td>Provincia </td> <td>$merci_province</td></tr>
+			<tr style=\"background-color: #dddddd;\"><td>Provincia </td> <td>$merci_province</td></tr>
 
 			<tr><td>Giorni di Chiusura</td> <td>$array_closing_day</td></tr>
-			<tr><td>Dettagli info giorni Chiusura</td> <td>$closing_day_details</td></tr>
+			<tr style=\"background-color: #dddddd;\"><td>Dettagli info giorni Chiusura</td> <td>$closing_day_details</td></tr>
 
 			<tr><td colspan='2'><h3>Dati Bancari</h3></td></tr>
-			<tr><td>Banca di Appoggio</td> <td>$banca_appoggio</td></tr>
+			<tr style=\"background-color: #dddddd;\"><td>Banca di Appoggio</td> <td>$banca_appoggio</td></tr>
 			<tr><td>Agenzia</td> <td>$banca_agenzia</td></tr>
-			<tr><td>IBAN</td> <td>$iban</td></tr>
+			<tr style=\"background-color: #dddddd;\"><td>IBAN</td> <td>$iban</td></tr>
 
 			<tr><td colspan='2'><h3>Dati Responsabile</h3></td></tr>
-			<tr><td>Responsabile</td> <td>$responsabile</td></tr>
+			<tr style=\"background-color: #dddddd;\"><td>Responsabile</td> <td>$responsabile</td></tr>
 
 			<tr><td>Note</td> <td>$message_note</td></tr>
 
-			<tr><td colspan='2'><h3>Presa visione sulla privacy</h3></td></tr>
+			<tr style=\"background-color: #dddddd;\"><td colspan='2'><h3>Presa visione sulla privacy</h3></td></tr>
 			<tr><td>relativamente al punto 1.A</td> <td>$privacy_accept_1</td></tr>
-			<tr><td>relativamente al punto 1.B</td> <td>$privacy_accept_2</td></tr>
+			<tr style=\"background-color: #dddddd;\"><td>relativamente al punto 1.B</td> <td>$privacy_accept_2</td></tr>
 			</table>
 		</body>
 	</html>";
